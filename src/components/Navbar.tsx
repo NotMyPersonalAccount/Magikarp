b2ec604c34bdea112fa36ea266ab1899b0a2a7c2
NotMyPersonalAccount <kevinzj710@gmail.com>
2021-05-08 01:20:51 -0700

    feat(pages): landing page & navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,23 @@ import { useSession, signIn, signOut } from "next-auth/client";
 export default function Navbar(): ReactElement {
 	const [session, loading] = useSession();
 	return (
-		<nav className="dark bg-gray-100">
-			<button onClick={() => (loading ? null : session ? signOut() : signIn())}>
-				{loading ? "Loading" : session ? "Logout" : "Login"}
-			</button>
+		<nav className="flex bg-gray-50 shadow-sm items-center">
+			<p className="text-2xl text-red-400 px-4 py-2 font-mono font-semibold">
+				magikarp
+			</p>
+			{loading ? null : session ? (
+				<img
+					className="cursor-pointer ml-auto rounded-full h-10 w-10 m-4"
+					src={session.user.image}
+					onClick={() => signOut()} //TODO: Quit being lazy and make a menu for settings/sign out
+				/>
+			) : (
+				<button
+					className="bg-red-500 hover:bg-red-600 focus:outline-none rounded-xl shadow-lg px-4 py-2 ml-auto mr-4 my-2 text-white text-bold"
+					onClick={() => signIn()}>
+					Login
+				</button>
+			)}
 		</nav>
-	); //TODO: Full & proper navbar
+	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
 import { ReactElement } from "react";
+import Link from "next/link";
 
 export default function Home(): ReactElement {
-	return <>Hello World</>;
+	return (
+		<header className="max-w-screen-lg xl:max-w-screen-xl mx-auto">
+			<div className="px-4 sm:px-6 md:px-8">
+				<div className="mt-12 mb-5">
+					<p className="text-4xl text-red-400 font-mono font-semibold">
+						magikarp
+					</p>
+				</div>
+				<h1 className="text-4xl sm:text-6xl lg:text-7xl font-extrabold mt-10 mb-8">
+					Easier attendance and communication for teachers & students.
+				</h1>
+				<p className="text-2xl my-2">
+					Submit attendance for all your classes. Communicate with your
+					classmates & teachers. All within one platform. For free. Created by a
+					moron with terrible design choices.
+				</p>
+				<button className="bg-red-500 hover:bg-red-600 focus:outline-none rounded-xl shadow-lg my-2 px-4 py-2 text-white text-bold">
+					<Link href="/home">Go to Home</Link>
+				</button>
+			</div>
+		</header>
+	);
 }
