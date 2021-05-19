8a9d65654e18243a3a789dacc596e6f7279bf4a6
NotMyPersonalAccount <kevinzj710@gmail.com>
2021-05-18 21:41:38 -0700

    style: improve look of central info component for small screens

--- src/components/central/CentralInfo.tsx
+++ src/components/central/CentralInfo.tsx
@@ -3,7 +3,7 @@ import { LayoutProps } from "../../types/props";
 
 export default function CentralInfo(props: LayoutProps): ReactElement {
 	return (
-		<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+		<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-8/12 sm:w-7/12 md:w-6/12 lg:w-5/12 xl:w-4/12">
 			<div className="bg-gray-100 rounded-md p-5 shadow-md overflow-hidden">
 				{props.children}
 			</div>
