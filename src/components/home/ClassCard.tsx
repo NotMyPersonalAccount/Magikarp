e877aeb72391782743346d21bcd26a4d4f175d09
NotMyPersonalAccount <kevinzj710@gmail.com>
2021-05-20 22:14:20 -0700

    style: more changes to class cards

--- src/components/home/ClassCard.tsx
+++ src/components/home/ClassCard.tsx
@@ -12,20 +12,20 @@ export default function ClassCard(props: ClassHolder): ReactElement {
 	return (
 		<div
 			key={_class.id}
-			className="flex mb-4 mr-2 lg:mr-4 p-4 bg-red-50 rounded-lg shadow-lg hover:shadow-xl w-80 h-40 transform hover:scale-105 hover:translate-y-2 transition duration-300 ease-in-out">
-			<div className="flex-grow min-w-0">
-				<Link href={`/class/${_class.id}`}>
-					<a className="text-xl">
-						<p className="overflow-ellipsis overflow-hidden whitespace-nowrap">
+			className="flex flex-col mb-4 mr-2 lg:mr-4 p-4 bg-red-50 rounded-lg shadow-lg hover:shadow-xl w-80 h-40 transform hover:scale-105 hover:translate-y-2 transition duration-300 ease-in-out">
+			<div className="flex flex-col justify-between flex-grow">
+				<div>
+					<Link href={`/class/${_class.id}`}>
+						<a className="text-xl hover:underline overflow-ellipsis overflow-hidden whitespace-nowrap">
 							{_class.name}
-						</p>
-					</a>
-				</Link>
+						</a>
+					</Link>
+				</div>
 				<p>{_class.description}</p>
 				<p>{teacher.name}</p>
 			</div>
 			<img
-				className="self-end rounded-full h-10 w-10 m-2"
+				className="self-end rounded-full h-10 w-10 mt-3"
 				src={teacher.image}
 			/>
 		</div>
